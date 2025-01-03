85c38810909c7adbdac23ecf134aa436596308a3
Vladimir Kosteley <github@ismd.dev>
2025-01-03 13:36:48 +0100

    feat: add sidebar for home page

--- themes/vg/assets/js/main.js
+++ themes/vg/assets/js/main.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.querySelector('.vg-hamburger');
     const sidebar = document.querySelector('.vg-sidebar');
+    const body = document.querySelector('body');
 
     if (hamburger && sidebar) {
         hamburger.addEventListener('click', () => {
+            body.classList.toggle('vg-sidebar-open');
             hamburger.classList.toggle('is-active');
             sidebar.classList.toggle('is-active');
         });
